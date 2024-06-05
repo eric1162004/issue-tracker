50b48cbb26dbb900a222d2e07d206dc546ca48ae
Eric Cheung <eric1162004@hotmail.com>
2024-06-05 15:40:30 -0700

    style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import classnames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { BsBugFill } from "react-icons/bs";
 
 const NavBar = () => {
+  const pathname = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -17,7 +23,11 @@ const NavBar = () => {
         {links.map((link) => (
           <li>
             <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classnames({
+                "text-zinc-900": link.href === pathname,
+                "text-zinc-500": link.href !== pathname,
+                "hover:text-zinc-800 transtion-colors": true,
+              })}
               href={link.href}
               key={link.href}
             >

152fb0c9be6a0ec6b383509480defd0ae4e6acfd
Eric Cheung <eric1162004@hotmail.com>
2024-06-06 02:52:03 -0700

    refractor: organsize import statments

---
+++ app/components/Skeleton.tsx
@@ -0,0 +1,6 @@
+import Skeleton from "react-loading-skeleton";
+
+// Skeleton require skeleton.css in to work properly
+import "react-loading-skeleton/dist/skeleton.css";
+
+export default Skeleton;

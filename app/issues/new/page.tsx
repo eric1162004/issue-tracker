6092120be93930cd7ad6a81f2dd94e9a28d6abc9
Eric Cheung <eric1162004@hotmail.com>
2024-06-05 23:52:48 -0700

    refactor: move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -41,6 +41,18 @@ const NewIssuePage = () => {
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setError("An unexpected error occurred.");
+    } finally {
+      setSubmitting(false);
+    }
+  });
+
   return (
     <div className="max-w-xl">
       {/* Show Unexpected Server Error */}
@@ -53,20 +65,7 @@ const NewIssuePage = () => {
         </Callout.Root>
       )}
 
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setError("An unexpected error occurred.");
-          } finally {
-            setSubmitting(false);
-          }
-        })}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>

279e2456849dc2c412382bacefbb232bfc2141c5
Eric Cheung <eric1162004@hotmail.com>
2024-06-07 14:10:24 -0700

    add metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -37,7 +37,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex direction={"column"} gap={"4"}>
-            <AssigneeSelect issue={issue}/>
+            <AssigneeSelect issue={issue} />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
@@ -46,5 +46,16 @@ const IssueDetailPage = async ({ params }: Props) => {
     </Grid>
   );
 };
+
+// Export dynamic metadata based on issue details
+export async function generateMetadata({ params }: Props) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  return {
+    title: issue?.title,
+    description: "Details of issue" + issue?.id,
+  };
+}
 
 export default IssueDetailPage;

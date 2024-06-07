c23030cc2922479cc58f1b37268ca659bf295038
Eric Cheung <eric1162004@hotmail.com>
2024-06-07 12:35:25 -0700

    build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { Text } from "@radix-ui/themes";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <>
+    <Text>hello</Text>
+    <Pagination itemCount={100} pageSize={10} currentPage={1}/>
+    </>
   )
 }

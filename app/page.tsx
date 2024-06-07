89225f6574f26a64bee7a0e96f9c0f2a480505e2
Eric Cheung <eric1162004@hotmail.com>
2024-06-07 13:47:35 -0700

    build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 import { Text } from "@radix-ui/themes";
 import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
-  return (<LatestIssues/>
+  const openCount = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgressCount = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closedCount = await prisma.issue.count({ where: { status: "CLOSED" } });
+
+  return (
+    <IssueSummary
+      open={openCount}
+      inProgress={inProgressCount}
+      closed={closedCount}
+    />
   );
 }

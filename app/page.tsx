47b2310c7358370466fe096c064c5c93d0ca24d8
Eric Cheung <eric1162004@hotmail.com>
2024-06-07 13:32:59 -0700

    build the LatestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import { Text } from "@radix-ui/themes";
 import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
 export default function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
-  return (
-    <>
-      <Text>hello</Text>
-      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
-    </>
+  return (<LatestIssues/>
   );
 }
